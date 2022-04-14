fdc4e6cf12645b31a33325ece90ba87a5efcbc5f
Desmond Hui <desmond@flipsidecrypto.com>
2022-04-14 11:34:12 -0700

    initial setup files

---
+++ models/bronze/bronze__blocks.sql
@@ -0,0 +1,19 @@
+{{ config (
+    materialized = 'view'
+) }}
+
+SELECT 
+    record_id, 
+    offset_id,
+    block_id,
+    block_timestamp, 
+    network, 
+    chain_id, 
+    tx_count, 
+    header, 
+    ingested_at
+FROM 
+    {{ source(
+      'prod',
+      'flow_blocks'
+    ) }} 
