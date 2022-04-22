d532fa55789239df3e1c01c89b6fe7abd05532c2
Jack Forgash <jmfxyz@pm.me>
2022-04-22 16:31:21 -0600

    hard coding silver txs temp

--- models/silver/silver__tx_events.sql
+++ models/silver/silver__tx_events.sql
@@ -7,10 +7,11 @@
     tags=['silver','transactions', 'events']
   )
 }}
+
 with
 transactions as (
 
-  select * from {{ ref('silver__transactions') }}
+  select * from flow_dev.silver.transactions
 
 ),
 
@@ -26,16 +27,24 @@ events as (
           when value:event_index is null then value:eventIndex::number
           else value:event_index::number
       end as event_index,
-      value:type::string as type,
-      value:value::variant as event_value,
+      split(value:type, '.') as type_split,
+      case
+        when array_size(type_split) = 4 then concat_ws('.', type_split[0], type_split[1], type_split[2])::string
+        else type_split[0]::string
+      end as contract,
+      case
+        when array_size(type_split) = 4 then type_split[3]::string
+        else type_split[1]::string
+      end as type,
+      value:value::variant as event_data,
       case
           when value:value:EventType is null then value:value:eventType::variant
           else value:value:EventType::variant
-      end as event_type,
+      end as event_data_type,
       case
           when value:value:Fields is null then value:value:fields::variant
           else value:value:Fields::variant
-      end as event_fields,
+      end as event_data_fields,
       concat_ws('-', tx_id, event_index) as event_id
 
   from transactions, lateral flatten(input => transaction_result:events)
@@ -52,10 +61,11 @@ final as (
     block_id,
     error_status,
     event_index,
+    contract,
     type,
-    event_value,
-    event_type,
-    event_fields
+    event_data,
+    event_data_type,
+    event_data_fields
 
   from events
 
