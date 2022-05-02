edd973be71a99d05b350b5c374cb4c56f4be667b
Jack Forgash <jmfxyz@pm.me>
2022-05-02 15:42:58 -0600

    tests, format, rename error column

--- models/silver/silver__transactions.sql
+++ models/silver/silver__transactions.sql
@@ -1,57 +1,58 @@
-{{
-  config(
-    materialized='incremental',
-    cluster_by='block_timestamp',
-    unique_key='tx_id',
-    incremental_strategy = 'delete+insert',
-    tags=['silver', 'txs']
-  )
-}}
+{{ config(
+  materialized = 'incremental',
+  cluster_by = ['ingested_at::DATE', 'block_timestamp::DATE'],
+  unique_key = 'tx_id',
+  incremental_strategy = 'delete+insert'
+) }}
 
-with
-bronze_txs as (
+WITH bronze_txs AS (
 
-  select * from {{ ref('bronze__transactions') }}
+  SELECT
+    *
+  FROM
+    {{ ref('bronze__transactions') }}
 
-  {% if is_incremental() %}
-    where ingested_at::date >= getdate() - interval '2 days'
-  {% endif %}
+{% if is_incremental() %}
+WHERE
+  ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
 
-  qualify row_number() over (partition by tx_id order by ingested_at desc) = 1
-
+qualify ROW_NUMBER() over (
+  PARTITION BY tx_id
+  ORDER BY
+    ingested_at DESC
+) = 1
 ),
-
-silver_txs as (
-
-  select
-
-      tx_id,
-      block_timestamp,
-      block_id as block_height,
-      chain_id,
-      case
-        when tx:proposal_key:Address is null then tx:proposalKeyAddress::string
-        else tx:proposal_key:Address::string
-      end as proposer,
-      tx:payer::string as payer,
-      tx:authorizers::array as authorizers,
-      array_size(authorizers) as count_authorizers,
-      case
-        when tx:gas_limit is null then tx:gasLimit::number
-        else tx:gas_limit::number
-      end as gas_limit,
-      case
-        when tx:transaction_result is null then tx:result::variant
-        else tx:transaction_result::variant
-      end as transaction_result,
-      case
-        when transaction_result:error = '{}' then 'FAILED'
-        else 'SUCCEEDED'
-      end as error_status,
-      ingested_at
-
-  from bronze_txs
-
+final AS (
+  SELECT
+    tx_id,
+    block_timestamp,
+    block_id AS block_height,
+    chain_id,
+    COALESCE(
+      tx :proposal_key :Address,
+      tx :proposalKeyAddress
+    ) :: STRING AS proposer,
+    tx :payer :: STRING AS payer,
+    tx :authorizers :: ARRAY AS authorizers,
+    ARRAY_SIZE(authorizers) AS count_authorizers,
+    COALESCE(
+      tx :gas_limit,
+      tx :gasLimit
+    ) :: NUMBER AS gas_limit,
+    COALESCE(
+      tx :transaction_result,
+      tx :result
+    ) :: variant AS transaction_result,
+    CASE
+      WHEN transaction_result :error = '{}' THEN false
+      ELSE true
+    END AS tx_succeeded,
+    ingested_at
+  FROM
+    bronze_txs
 )
-
-select * from silver_txs
+SELECT
+  *
+FROM
+  final
