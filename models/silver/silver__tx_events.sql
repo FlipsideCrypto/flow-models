0955b9e90370ded2bab6db3b588434fc43e97cb0
Jack Forgash <jmfxyz@pm.me>
2022-05-02 16:44:17 -0600

    coalesce, rename type to event_type

--- models/silver/silver__tx_events.sql
+++ models/silver/silver__tx_events.sql
@@ -1,17 +1,16 @@
 {{
   config(
     materialized='incremental',
-    cluster_by='block_timestamp',
+    cluster_by=['ingested_at::DATA', 'block_timestamp::DATA'],
     unique_key='event_id',
-    incremental_strategy = 'delete+insert',
-    tags=['silver','transactions', 'events']
+    incremental_strategy = 'delete+insert'
   )
 }}
 
 with
 transactions as (
 
-  select * from flow_dev.silver.transactions
+  select * from {{ ref('silver__transactions') }}
 
 ),
 
@@ -23,29 +22,21 @@ events as (
       block_timestamp,
       block_id,
       error_status,
-      case
-          when value:event_index is null then value:eventIndex::number
-          else value:event_index::number
-      end as event_index,
+      coalesce(value:event_index, value:eventIndex)::number as event_index,
       split(value:type, '.') as type_split,
       case
         when array_size(type_split) = 4 then concat_ws('.', type_split[0], type_split[1], type_split[2])::string
         else type_split[0]::string
-      end as contract,
+      end as event_contract,
       case
         when array_size(type_split) = 4 then type_split[3]::string
         else type_split[1]::string
-      end as type,
+      end as event_type,
       value:value::variant as event_data,
-      case
-          when value:value:EventType is null then value:value:eventType::variant
-          else value:value:EventType::variant
-      end as event_data_type,
-      case
-          when value:value:Fields is null then value:value:fields::variant
-          else value:value:Fields::variant
-      end as event_data_fields,
-      concat_ws('-', tx_id, event_index) as event_id
+      coalesce(value:value:EventType, value:value:eventType)::variant as event_data_type,
+      coalesce(value:value:Fields, value:value:fields)::variant as event_data_fields,
+      concat_ws('-', tx_id, event_index) as event_id,
+      ingested_at
 
   from transactions, lateral flatten(input => transaction_result:events)
 
@@ -61,11 +52,12 @@ final as (
     block_id,
     error_status,
     event_index,
-    contract,
-    type,
+    event_contract,
+    event_type,
     event_data,
     event_data_type,
-    event_data_fields
+    event_data_fields,
+    ingested_at
 
   from events
 
