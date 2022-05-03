79b10d6c0f6af20236429c55a8299a17564417e2
Jack Forgash <jmfxyz@pm.me>
2022-05-03 15:53:42 -0600

    formatting

--- models/silver/silver__tx_events.sql
+++ models/silver/silver__tx_events.sql
@@ -1,51 +1,72 @@
-{{
-  config(
-    materialized='incremental',
-    cluster_by=['ingested_at::DATE', 'block_timestamp::DATE'],
-    unique_key='event_id',
-    incremental_strategy = 'delete+insert'
-  )
-}}
+{{ config(
+  materialized = 'incremental',
+  cluster_by = ['ingested_at::DATE', 'block_timestamp::DATE'],
+  unique_key = 'event_id',
+  incremental_strategy = 'delete+insert'
+) }}
 
-with
-transactions as (
+WITH transactions AS (
 
-  select * from {{ ref('silver__transactions') }}
+  SELECT
+    *
+  FROM
+    {{ ref('silver__transactions') }}
 
+{% if is_incremental() %}
+WHERE
+  ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
 ),
-
-events as (
-
-  select
-
+events AS (
+  SELECT
+    tx_id,
+    block_timestamp,
+    block_height,
+    tx_succeeded,
+    COALESCE(
+      VALUE :event_index,
+      VALUE :eventIndex
+    ) :: NUMBER AS event_index,
+    SPLIT(
+      VALUE :type,
+      '.'
+    ) AS type_split,
+    CASE
+      WHEN ARRAY_SIZE(type_split) = 4 THEN concat_ws(
+        '.',
+        type_split [0],
+        type_split [1],
+        type_split [2]
+      ) :: STRING
+      ELSE type_split [0] :: STRING
+    END AS event_contract,
+    CASE
+      WHEN ARRAY_SIZE(type_split) = 4 THEN type_split [3] :: STRING
+      ELSE type_split [1] :: STRING
+    END AS event_type,
+    VALUE :value :: variant AS event_data,
+    COALESCE(
+      VALUE :value :EventType,
+      VALUE :value :eventType
+    ) :: variant AS event_data_type,
+    COALESCE(
+      VALUE :value :Fields,
+      VALUE :value :fields
+    ) :: variant AS event_data_fields,
+    concat_ws(
+      '-',
       tx_id,
-      block_timestamp,
-      block_height,
-      tx_succeeded,
-      coalesce(value:event_index, value:eventIndex)::number as event_index,
-      split(value:type, '.') as type_split,
-      case
-        when array_size(type_split) = 4 then concat_ws('.', type_split[0], type_split[1], type_split[2])::string
-        else type_split[0]::string
-      end as event_contract,
-      case
-        when array_size(type_split) = 4 then type_split[3]::string
-        else type_split[1]::string
-      end as event_type,
-      value:value::variant as event_data,
-      coalesce(value:value:EventType, value:value:eventType)::variant as event_data_type,
-      coalesce(value:value:Fields, value:value:fields)::variant as event_data_fields,
-      concat_ws('-', tx_id, event_index) as event_id,
-      ingested_at
-
-  from transactions, lateral flatten(input => transaction_result:events)
-
+      event_index
+    ) AS event_id,
+    ingested_at
+  FROM
+    transactions,
+    LATERAL FLATTEN(
+      input => transaction_result :events
+    )
 ),
-
-final as (
-
-  select
-
+FINAL AS (
+  SELECT
     event_id,
     tx_id,
     block_timestamp,
@@ -55,12 +76,13 @@ final as (
     event_contract,
     event_type,
     event_data,
-    event_data_type,
-    event_data_fields,
-    ingested_at
-
-  from events
-
+    event_data_type AS _event_data_type,
+    event_data_fields AS _event_data_fields,
+    ingested_at AS _ingested_at
+  FROM
+    events
 )
-
-select * from final
+SELECT
+  *
+FROM
+  FINAL
