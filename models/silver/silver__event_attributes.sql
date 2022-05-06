3b901f966cf97b4b8d95f46f48f1576e031bca59
Jack Forgash <jmfxyz@pm.me>
2022-05-05 18:08:45 -0600

    fixed upstream

--- models/silver/silver__event_attributes.sql
+++ models/silver/silver__event_attributes.sql
@@ -1,25 +1,24 @@
 {{ config(
-  materialized = 'incremental',
-  cluster_by = ['_ingested_at::DATE', 'block_timestamp::DATE'],
-  unique_key = 'attribute_id',
-  incremental_strategy = 'delete+insert'
+    materialized = 'incremental',
+    cluster_by = ['_ingested_at::DATE', 'block_timestamp::DATE'],
+    unique_key = 'attribute_id',
+    incremental_strategy = 'delete+insert'
 ) }}
 
-with 
-events as (
+WITH events AS (
 
-    select * from {{ ref('silver__events') }}
+    SELECT
+        *
+    FROM
+        {{ ref('silver__events') }}
 
-    {% if is_incremental() %}
-        WHERE
-            _ingested_at :: DATE >= CURRENT_DATE - 2
-    {% endif %}
-
+{% if is_incremental() %}
+WHERE
+    _ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
 ),
-
-events_data as (
-    select
-
+events_data AS (
+    SELECT
         event_id,
         tx_id,
         block_timestamp,
@@ -29,64 +28,73 @@ events_data as (
         _event_data_type,
         _event_data_fields,
         _ingested_at,
-        coalesce(_event_data_type:fields, _event_data_type:Fields) as event_data_type_fields
-
-    from events
-    
+        COALESCE(
+            _event_data_type :fields,
+            _event_data_type :Fields
+        ) AS event_data_type_fields
+    FROM
+        events
 ),
-
-attributes as (
-
-    select
-
+attributes AS (
+    SELECT
         event_id,
         tx_id,
         block_timestamp,
         event_index,
         event_contract,
         event_type,
-        coalesce(value:identifier, value:Identifier)::string as attribute_key,
-        coalesce(_event_data_fields[index]:Value, _event_data_fields[index]) as attribute_value,
-        concat_ws('-', event_id, index) as attribute_id,
-        index as attribute_index,
+        COALESCE(
+            VALUE :identifier,
+            VALUE :Identifier
+        ) :: STRING AS attribute_key,
+        COALESCE(
+            _event_data_fields [index] :Value,
+            _event_data_fields [index]
+        ) AS attribute_value,
+        concat_ws(
+            '-',
+            event_id,
+            INDEX
+        ) AS attribute_id,
+        INDEX AS attribute_index,
         _ingested_at
-
-    from events_data, lateral flatten(input => event_data_type_fields)
-
+    FROM
+        events_data,
+        LATERAL FLATTEN(
+            input => event_data_type_fields
+        )
 ),
-
-handle_address_arrays as (
-
-    select
-
+handle_address_arrays AS (
+    SELECT
         attribute_id,
         b.index,
-        lpad(trim(to_char(b.value::int,'XXXXXXX'))::string,2,'0') as hex
-
-    from attributes a, table(flatten(attribute_value, recursive=>true)) b
-
-    where is_array(attribute_value) = true
-    order by 1,2
-
+        LPAD(TRIM(to_char(b.value :: INT, 'XXXXXXX')) :: STRING, 2, '0') AS hex
+    FROM
+        attributes A,
+        TABLE(FLATTEN(attribute_value, recursive => TRUE)) b
+    WHERE
+        IS_ARRAY(attribute_value) = TRUE
+    ORDER BY
+        1,
+        2
 ),
-
-recombine_address as (
-
-    select
-
+recombine_address AS (
+    SELECT
         attribute_id,
-        concat('0x',array_to_string(array_agg(hex) within group (order by index asc),'')) as decoded_address
-
-    from handle_address_arrays
-    group by 1
-
+        CONCAT(
+            '0x',
+            ARRAY_TO_STRING(ARRAY_AGG(hex) within GROUP (
+            ORDER BY
+                INDEX ASC), '')
+        ) AS decoded_address
+    FROM
+        handle_address_arrays
+    GROUP BY
+        1
 ),
-
-replace_arrays as (
-
-    select
-
-        a.attribute_id,
+replace_arrays AS (
+    SELECT
+        A.attribute_id,
         event_id,
         tx_id,
         block_timestamp,
@@ -97,34 +105,32 @@ replace_arrays as (
         attribute_key,
         attribute_value,
         decoded_address,
-        coalesce(decoded_address, attribute_value)::string as attribute_value_adj,
+        COALESCE(
+            decoded_address,
+            attribute_value
+        ) :: STRING AS attribute_value_adj,
         _ingested_at
-
-    from attributes a
-        left join recombine_address using (attribute_id)
-
+    FROM
+        attributes A
+        LEFT JOIN recombine_address USING (attribute_id)
 ),
-
-address_adjustment as (
-
-    select 
-
+address_adjustment AS (
+    SELECT
         attribute_id,
-        length(attribute_value_adj) as ava_len, 
-        concat('0x',lpad(split(attribute_value_adj, '0x')[1],16,'0')::string) as address_adj
-
-    from replace_arrays
-    where attribute_value_adj like '0x%'
-        and ava_len < 19
-
-
+        LENGTH(attribute_value_adj) AS ava_len,
+        CONCAT(
+            '0x',
+            LPAD(SPLIT(attribute_value_adj, '0x') [1], 16, '0') :: STRING
+        ) AS address_adj
+    FROM
+        replace_arrays
+    WHERE
+        attribute_value_adj LIKE '0x%'
+        AND ava_len < 19
 ),
-
-final as (
-
-    select
-    
-        a.attribute_id,
+FINAL AS (
+    SELECT
+        A.attribute_id,
         event_id,
         tx_id,
         block_timestamp,
@@ -135,12 +141,16 @@ final as (
         attribute_key,
         decoded_address,
         attribute_value,
-        coalesce(address_adj, attribute_value_adj) as attribute_value_adj,
+        COALESCE(
+            address_adj,
+            attribute_value_adj
+        ) AS attribute_value_adj,
         _ingested_at
-
-    from replace_arrays a
-    left join address_adjustment using (attribute_id)
-
+    FROM
+        replace_arrays A
+        LEFT JOIN address_adjustment USING (attribute_id)
 )
-
-select * from final
+SELECT
+    *
+FROM
+    FINAL
