eccc1cd9802afc2bcce792a9b7aed86e9d39e662
Jack Forgash <jmfxyz@pm.me>
2022-06-03 14:08:41 -0600

    nft purchase txs v1

---
+++ models/silver/silver__nft_transactions_v1.sql
@@ -0,0 +1,92 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    cluster_by = ['_ingested_at::DATE, block_timestamp::DATE'],
+    unique_key = 'tx_id'
+) }}
+
+WITH silver_events AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('silver__events_final') }}
+
+{% if is_incremental() %}
+WHERE
+    _ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
+),
+listing_data AS (
+    SELECT
+        *,
+        event_contract AS marketplace_contract,
+        event_data :nftID :: STRING AS nft_id_listing,
+        event_data :nftType :: STRING AS nft_collection_listing,
+        event_data :purchased :: BOOLEAN AS purchased -- false indicates a listing was cancelled while true is when the order was executed
+    FROM
+        silver_events
+    WHERE
+        event_type = 'ListingCompleted'
+        AND event_contract = 'A.4eb8a10cb9f87357.NFTStorefront' -- general storefront
+),
+purchase_data AS (
+    SELECT
+        tx_id,
+        event_contract AS currency,
+        event_data :amount :: NUMBER AS amount,
+        event_data :from :: STRING AS buyer_purchase
+    FROM
+        silver_events
+    WHERE
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                listing_data
+        )
+        AND event_index = 0
+),
+seller_data AS (
+    SELECT
+        tx_id,
+        event_contract AS nft_collection_seller,
+        event_data :from :: STRING AS seller,
+        event_data :id :: STRING AS nft_id_seller
+    FROM
+        silver_events
+    WHERE
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                listing_data
+        )
+        AND event_type = 'Withdraw'
+),
+deposit_data AS (
+    SELECT
+        tx_id,
+        event_contract AS nft_collection_deposit,
+        event_data :id :: STRING AS nft_id_deposit,
+        event_data :to :: STRING AS buyer_deposit
+    FROM
+        silver_events
+    WHERE
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                listing_data
+        )
+        AND event_type = 'Deposit'
+)
+SELECT
+    *
+FROM
+    listing_data
+    LEFT JOIN purchase_data USING (tx_id)
+    LEFT JOIN seller_data USING (tx_id)
+    LEFT JOIN deposit_data USING (tx_id)
+WHERE
+    purchased = TRUE
