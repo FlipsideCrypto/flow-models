7d29b52af42996f594ce0420ecf7d305dd8e9803
Jack Forgash <jmfxyz@pm.me>
2022-06-08 10:16:12 -0600

    blocto transactions

---
+++ models/silver/silver__transactions_blocto.sql
@@ -0,0 +1,31 @@
+{{ config(
+    materialized = 'incremental',
+    cluster_by = ['_ingested_at::DATE', 'block_timestamp::DATE'],
+    unique_key = 'tx_id',
+    incremental_strategy = 'delete+insert'
+) }}
+
+WITH silver_txs AS (
+
+    SELECT
+        *
+    FROM
+        {{ ref('silver__transactions') }}
+
+{% if is_incremental() %}
+WHERE
+    _ingested_at :: DATE >= CURRENT_DATE - 2
+{% endif %}
+),
+blocto_txs AS (
+    SELECT
+        *
+    FROM
+        silver_txs
+    WHERE
+        LOWER(proposer) = LOWER('0x55AD22F01EF568A1')
+)
+SELECT
+    *
+FROM
+    blocto_txs
