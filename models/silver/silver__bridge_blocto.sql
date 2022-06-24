4a9fa12dd40610648ab2a60a3c79b7781358d3b6
forgash_ <jmfxyz@pm.me>
2022-06-24 14:52:32 -0600

    blocto teleports final

--- models/silver/silver__bridge_blocto.sql
+++ models/silver/silver__bridge_blocto.sql
@@ -17,11 +17,24 @@ WHERE
     _ingested_at :: DATE >= CURRENT_DATE -2
 {% endif %}
 ),
-teleports AS (
+teleport_events AS (
     SELECT
         tx_id,
         block_timestamp,
         block_height,
+        event_contract AS teleport_contract_fee,
+        event_data :amount :: DOUBLE AS amount_fee,
+        event_data :type :: NUMBER AS teleport_direction,
+        _ingested_at
+    FROM
+        events
+    WHERE
+        event_type = 'FeeCollected'
+        AND event_contract LIKE '%Teleport%'
+),
+teleports_in AS (
+    SELECT
+        tx_id,
         event_contract AS event_contract_teleport,
         event_data :amount :: DOUBLE AS amount_teleport,
         event_data :from AS from_teleport,
@@ -37,29 +50,21 @@ teleports AS (
     FROM
         events
     WHERE
-        event_index = 0
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                teleport_events
+            WHERE
+                teleport_direction = 1
+        )
+        AND event_index = 0
         AND event_contract LIKE '%Teleport%'
         AND event_type IN (
             'TokensTeleportedIn',
             'Unlocked'
         )
         AND from_teleport :: STRING NOT LIKE '%{\"ArrayType%'
-),
-fees AS (
-    SELECT
-        tx_id,
-        event_data :amount :: DOUBLE AS amount_fee,
-        event_data :type :: NUMBER AS type_fee
-    FROM
-        events
-    WHERE
-        tx_id IN (
-            SELECT
-                tx_id
-            FROM
-                teleports
-        )
-        AND event_type = 'FeeCollected'
 ),
 deposits AS (
     SELECT
@@ -74,13 +79,13 @@ deposits AS (
         ) AS rn,
         event_data :to :: STRING AS to_deposits
     FROM
-        flow_dev.silver.events_final
+        events
     WHERE
         tx_id IN (
             SELECT
                 tx_id
             FROM
-                teleports
+                teleports_in
         )
         AND event_type = 'TokensDeposited'
     ORDER BY
@@ -90,24 +95,120 @@ deposits AS (
 blocto_inbound AS (
     SELECT
         t.tx_id,
-        t.block_timestamp,
-        t.block_height,
+        f.block_timestamp,
+        f.block_height,
         t.event_contract_teleport AS teleport_contract,
         d.event_contract AS token_contract,
-        t.amount_teleport,
+        t.amount_teleport AS gross_amount,
         f.amount_fee,
         d.amount_deposits AS net_amount,
         d.to_deposits AS flow_wallet_address,
-        'inbound' AS direction,
-        'blocto' AS bridge
+        f.teleport_direction,
+        'blocto' AS bridge,
+        f._ingested_at
     FROM
-        teleports t
+        teleports_in t
         LEFT JOIN deposits d USING (tx_id)
-        LEFT JOIN fees f USING (tx_id)
+        LEFT JOIN teleport_events f USING (tx_id)
     WHERE
         d.rn = 1
+),
+teleports_out_withdraw AS (
+    SELECT
+        tx_id,
+        event_contract,
+        event_data :amount :: DOUBLE AS amount_withdraw,
+        event_data :from :: STRING AS from_withdraw
+    FROM
+        events
+    WHERE
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                teleport_events
+            WHERE
+                teleport_direction = 0
+        )
+        AND event_index = 0
+),
+teleports_out AS (
+    SELECT
+        tx_id,
+        event_contract AS event_contract_teleport,
+        event_data :amount :: DOUBLE AS amount_teleport,
+        event_data :to AS to_teleport,
+        STRTOK_TO_ARRAY(
+            REPLACE(REPLACE(event_data :to :: STRING, '['), ']'),
+            ', '
+        ) :: ARRAY AS to_teleport_array
+    FROM
+        events
+    WHERE
+        tx_id IN (
+            SELECT
+                tx_id
+            FROM
+                teleport_events
+            WHERE
+                teleport_direction = 0
+        )
+        AND event_type IN (
+            'TokensTeleportedOut',
+            'Locked'
+        )
+),
+blocto_outbound AS (
+    SELECT
+        t.tx_id,
+        f.block_timestamp,
+        f.block_height,
+        t.event_contract_teleport AS teleport_contract,
+        w.event_contract AS token_contract,
+        w.amount_withdraw AS gross_amount,
+        f.amount_fee,
+        t.amount_teleport AS net_amount,
+        w.from_withdraw AS flow_wallet_address,
+        f.teleport_direction,
+        'blocto' AS bridge,
+        f._ingested_at
+    FROM
+        teleports_out t
+        LEFT JOIN teleports_out_withdraw w USING (tx_id)
+        LEFT JOIN teleport_events f USING (tx_id)
+),
+tbl_union AS (
+    SELECT
+        *
+    FROM
+        blocto_inbound
+    UNION
+    SELECT
+        *
+    FROM
+        blocto_outbound
+),
+FINAL AS (
+    SELECT
+        tx_id,
+        block_timestamp,
+        block_height,
+        teleport_contract,
+        token_contract,
+        gross_amount,
+        amount_fee,
+        net_amount,
+        flow_wallet_address,
+        CASE
+            WHEN teleport_direction = 0 THEN 'outbound'
+            ELSE 'inbound'
+        END AS teleport_direction,
+        bridge,
+        _ingested_at
+    FROM
+        tbl_union
 )
 SELECT
     *
 FROM
-    blocto_inbound
+    FINAL
