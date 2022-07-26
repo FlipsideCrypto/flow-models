f93bc4aadc64339bffb013158f136b61a6acffa4
Jack Forgash <jmfxyz@pm.me>
2022-07-26 15:13:12 -0600

    caps for comment

--- models/core/core__dim_topshot_metadata.sql
+++ models/core/core__dim_topshot_metadata.sql
@@ -5,7 +5,24 @@
 WITH topshot AS (
 
     SELECT
-        *
+        nft_id,
+        nft_collection,
+        nbatopshot_id,
+        serial_number,
+        total_circulation,
+        moment_description,
+        player,
+        team,
+        season,
+        play_category,
+        play_type,
+        moment_date,
+        set_name,
+        set_series_number,
+        video_urls,
+        moment_stats_full,
+        player_stats_game,
+        player_stats_season_to_date,
     FROM
         {{ ref('silver__nft_topshot_metadata') }}
 )
