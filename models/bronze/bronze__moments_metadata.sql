70e9402be9eee060cf9d087c9aef9ff19567fcd2
forgash_ <jmfxyz@pm.me>
2022-08-29 10:53:50 -0600

    allday metadata silver

--- models/bronze/bronze__moments_metadata.sql
+++ models/bronze/bronze__moments_metadata.sql
@@ -7,11 +7,12 @@ SELECT
     contract,
     DATA,
     VALUE,
-    TO_TIMESTAMP_NTZ(SUBSTR(SPLIT_PART(metadata$filename, '/', 4), 1, 10) :: NUMBER, 0) AS _inserted_timestamp
+    TO_TIMESTAMP_NTZ(
+        SUBSTR(SPLIT_PART(metadata $ filename, '/', 4), 1, 10) :: NUMBER,
+        0
+    ) AS _inserted_timestamp
 FROM
     {{ source(
         'flow_external',
         'moments_metadata_api'
     ) }}
-WHERE
-    contract = 'A.0b2a3299cc857e29.TopShot'
