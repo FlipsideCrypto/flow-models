2958c61af213aa4b4cef4b4ad17f23b04af3ea2b
Jack Forgash <jmfxyz@pm.me>
2022-09-13 00:34:27 +0100

    excl errors

--- models/silver/silver__nft_transactions_secondary_market.sql
+++ models/silver/silver__nft_transactions_secondary_market.sql
@@ -12,15 +12,16 @@ WITH silver_events AS (
         *
     FROM
         {{ ref('silver__events_final') }}
+    WHERE
+        event_data :: STRING != '{}'
 
 {% if is_incremental() %}
-WHERE
-    _inserted_timestamp >= (
-        SELECT
-            MAX(_inserted_timestamp)
-        FROM
-            {{ this }}
-    )
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp)
+    FROM
+        {{ this }}
+)
 {% endif %}
 ),
 sale_trigger AS (
