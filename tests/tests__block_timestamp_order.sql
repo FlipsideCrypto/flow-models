8a5af8de93c48ed5d1ece1e593c7f4fbf4e180f1
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2022-12-12 14:32:49 -0700

    Block order test (#96)
    * bespoke test
    
    * severity

---
+++ tests/tests__block_timestamp_order.sql
@@ -0,0 +1,20 @@
+{{ config(
+    severity = "error"
+) }}
+
+SELECT
+    DATEDIFF(
+        SECOND,
+        A.block_timestamp,
+        b.block_timestamp
+    ) AS avg_time_diff,
+    b.block_height AS bheight,
+    b.block_timestamp AS btime,
+    A.*
+FROM
+    {{ ref('silver__blocks') }} A,
+    {{ ref('silver__blocks') }}
+    b
+WHERE
+    A.block_height = b.block_height -1
+    AND avg_time_diff < 0
