5b3f0b363d600b5e8693dd13a6459dd9504eebad
Desmond Hui <desmond@flipsidecrypto.com>
2022-12-13 12:02:56 -0800

    model api responses that resulted in no metadata

---
+++ models/streamline/streamline__null_moments_metadata.sql
@@ -0,0 +1,34 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = ["id","contract","_inserted_date"]
+) }}
+
+SELECT
+    id,
+    contract,
+    _inserted_date,
+    TO_TIMESTAMP_NTZ(SUBSTR(SPLIT_PART(metadata$filename, '/', 4), 1, 10) :: NUMBER, 0) AS _inserted_timestamp
+FROM
+    {{ source(
+        'bronze_streamline',
+        'moments_minted_metadata_api'
+    ) }}
+WHERE
+    DATA :getMintedMoment :: STRING IS NULL
+
+{% if is_incremental() %}
+AND _inserted_date >= (
+    SELECT
+        MAX(_inserted_date)
+    FROM
+        {{ this }}
+)
+AND _inserted_timestamp > (
+    SELECT
+        MAX(_inserted_date)
+    FROM
+        {{ this }}
+)
+{% else %}
+AND _inserted_date >= '2022-12-09'
+{% endif %}
