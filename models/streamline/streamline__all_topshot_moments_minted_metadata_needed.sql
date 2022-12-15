e5863f899aa37ebde5ad04522b786b23ede9ce96
desmond-hui <97470747+desmond-hui@users.noreply.github.com>
2022-12-14 17:05:19 -0800

    An 2569/only retry req 3 times (#98)
    * model api responses that resulted in no metadata
    
    * dont try to get metadata after 3 retries
    
    * fix PR comment
    
    * use bronze model per PR

--- models/streamline/streamline__all_topshot_moments_minted_metadata_needed.sql
+++ models/streamline/streamline__all_topshot_moments_minted_metadata_needed.sql
@@ -34,14 +34,37 @@ all_topshots AS (
         moment_id
     FROM
         sales
+),
+always_null AS (
+    SELECT
+        id,
+        contract,
+        COUNT(*) AS num_times_null_resp
+    FROM
+        {{ ref('streamline__null_moments_metadata') }}
+    WHERE
+        contract = 'A.0b2a3299cc857e29.TopShot'
+    GROUP BY
+        1,
+        2
+    HAVING
+        num_times_null_resp > 2
 )
 SELECT
     DISTINCT *
 FROM
     all_topshots
 EXCEPT
-SELECT
-    nft_collection as event_contract,
-    nft_id AS moment_id
-FROM
-    {{ ref('silver__nft_topshot_metadata') }}
+    (
+        SELECT
+            nft_collection AS event_contract,
+            nft_id AS moment_id
+        FROM
+            {{ ref('silver__nft_topshot_metadata') }}
+        UNION
+        SELECT
+            contract,
+            id
+        FROM
+            always_null
+    )
