6d66eadec832658e3303a3f3310a7eb313a0956b
Jack Forgash <jmfxyz@pm.me>
2023-06-06 11:32:04 -0600

    move_block

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,15 +1,13 @@
 {% macro create_udfs() %}
-    {% if target.database != "FLOW_COMMUNITY_DEV" %}
-        {% set sql %}
-        {{ udf_bulk_get_topshot_moments_minted_metadata() }};
-        {{ udf_bulk_get_nfl_allday_moments_metadata() }};
-        {% endset %}
-        {% do run_query(sql) %}
-
-
-    {% endif %}
-
     {% if var("UPDATE_UDFS_AND_SPS") %}
-        {{- fsc_utils.create_udfs() -}}
+        {% if target.database != "FLOW_COMMUNITY_DEV" %}
+            {% set sql %}
+            {{ udf_bulk_get_topshot_moments_minted_metadata() }};
+            {{ udf_bulk_get_nfl_allday_moments_metadata() }};
+            {% endset %}
+            {% do run_query(sql) %}
+
+            {{- fsc_utils.create_udfs() -}}
+        {% endif %}
     {% endif %}
 {% endmacro %}
