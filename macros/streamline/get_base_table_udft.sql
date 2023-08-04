f12594a0df04b1df3ddbd3c529c9233631363f8a
shah <info@shahnewazkhan.ca>
2023-08-04 09:47:23 -0700

    node url join optimized | warehouse privilege grants

--- macros/streamline/get_base_table_udft.sql
+++ macros/streamline/get_base_table_udft.sql
@@ -3,15 +3,26 @@ create or replace function {{ schema }}.udtf_get_base_table(max_height integer)
 returns table (height number)
 as
 $$
-with base as (
-    select
-        row_number() over (
-            order by
-                seq4()
-        ) as height
-    from
-        table(generator(rowcount => 100000000))
-)
+    with base as (
+        select
+            row_number() over (
+                order by
+                    seq4()
+            ) as id
+        from
+            table(generator(rowcount => 100000000)) -- July 2023 Flow Chain head is at  57M
+    ),
+    node_mapping as (
+        select
+            base.id as height,
+            nv.node_url as node_url
+        from
+            base
+        left join flow_dev.seeds.network_version nv
+        on
+            base.id >= nv.root_height
+            and base.id <= nv.end_height
+    )
 select
     height 
 from
