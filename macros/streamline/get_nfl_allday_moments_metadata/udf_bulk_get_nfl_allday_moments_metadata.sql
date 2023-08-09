8a238d3bf5abf9006cefabfd0b78165ab4ea80ef
Jack Forgash <jmfxyz@pm.me>
2023-08-09 12:16:00 -0400

    del old streamline macros

--- macros/streamline/get_nfl_allday_moments_metadata/udf_bulk_get_nfl_allday_moments_metadata.sql
+++
@@ -1,8 +0,0 @@
-{% macro udf_bulk_get_nfl_allday_moments_metadata() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_nfl_allday_moments_metadata() returns text api_integration = aws_flow_api_dev AS {% if target.database == "FLOW" -%}
-        'https://3ltti6kisi.execute-api.us-east-1.amazonaws.com/prod/bulk_get_nfl_allday_metadata'
-    {% else %}
-        'https://wn6lmi2rs4.execute-api.us-east-1.amazonaws.com/dev/bulk_get_nfl_allday_metadata'
-    {%- endif %}
-{% endmacro %}
--- macros/streamline/get_topshot_moments_metadata/udf_bulk_get_topshot_moments_minted_metadata.sql
+++
@@ -1,8 +0,0 @@
-{% macro udf_bulk_get_topshot_moments_minted_metadata() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_get_topshot_moments_minted_metadata() returns text api_integration = aws_flow_api_dev AS {% if target.database == "FLOW" -%}
-        'https://3ltti6kisi.execute-api.us-east-1.amazonaws.com/prod/bulk_get_topshot_moments_minted_metadata'
-    {% else %}
-        'https://wn6lmi2rs4.execute-api.us-east-1.amazonaws.com/dev/bulk_get_topshot_moments_minted_metadata'
-    {%- endif %}
-{% endmacro %}
