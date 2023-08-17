74f8231eee83243b74067138afbe525d7d07cfda
shah <info@shahnewazkhan.ca>
2023-08-17 08:12:13 -0700

    removed node mapping logic

--- macros/streamline/get_base_table_udft.sql
+++ macros/streamline/get_base_table_udft.sql
@@ -11,17 +11,6 @@ $$
             ) as id
         from
             table(generator(rowcount => 100000000)) -- July 2023 Flow Chain head is at  57M
-    ),
-    node_mapping as (
-        select
-            base.id as height,
-            nv.node_url as node_url
-        from
-            base
-        left join flow_dev.seeds.network_version nv
-        on
-            base.id >= nv.root_height
-            and base.id <= nv.end_height
     )
 select
     height 
