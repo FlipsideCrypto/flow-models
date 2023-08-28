62b1c42e79fd03f9ca601b662ace79e238b91ee5
Jack Forgash <jmfxyz@pm.me>
2023-08-28 17:58:03 -0600

    rename silver dbt models to streamline_ and move into silver/core

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -6,7 +6,7 @@
     {% if target.name == "prod" %} 
         aws_flow_api_prod AS 'https://quxfxtl934.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% elif target.name == "dev" %}
-        aws_flow_api_dev_2 AS 'https://8jjulyhxhj.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+        aws_flow_api_dev_2 AS 'https://ul6x832e8l.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {% elif  target.name == "sbx" %}
         {{ log("Creating sbx get_chainhead", info=True) }}
         aws_flow_api_sbx AS 'https://bc5ejedoq8.execute-api.us-east-1.amazonaws.com/sbx/get_chainhead'
@@ -21,7 +21,7 @@
     {% if target.name == "prod" %} 
         aws_flow_api_prod AS 'https://quxfxtl934.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_grpc'
     {% elif target.name == "dev" %}
-        aws_flow_api_dev_2 AS 'https://8jjulyhxhj.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_grpc'
+        aws_flow_api_dev_2 AS 'https://ul6x832e8l.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_grpc'
     {% elif  target.name == "sbx" %}
         {{ log("Creating sbx udf_bulk_grpc", info=True) }}
         aws_flow_api_sbx AS 'https://bc5ejedoq8.execute-api.us-east-1.amazonaws.com/sbx/udf_bulk_grpc'
@@ -43,7 +43,7 @@
     {% if target.name == "prod" %} 
         aws_flow_api_prod AS 'https://quxfxtl934.execute-api.us-east-1.amazonaws.com/prod/udf_api'
     {% elif target.name == "dev" %}
-        aws_flow_api_dev_2 AS 'https://8jjulyhxhj.execute-api.us-east-1.amazonaws.com/dev/udf_api'
+        aws_flow_api_dev_2 AS 'https://ul6x832e8l.execute-api.us-east-1.amazonaws.com/dev/udf_api'
     {% elif  target.name == "sbx" %}
         {{ log("Creating sbx udf_api", info=True) }}
         aws_flow_api_sbx AS 'https://bc5ejedoq8.execute-api.us-east-1.amazonaws.com/sbx/udf_api'
