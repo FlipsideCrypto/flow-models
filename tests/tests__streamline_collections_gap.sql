1f4f34d69147db321db19544055d491f821ebb1f
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-09-05 12:51:35 -0600

    add missing collection array

--- tests/tests__streamline_collections_gap.sql
+++ tests/tests__streamline_collections_gap.sql
@@ -7,11 +7,13 @@ WITH collections_expected AS (
 
     SELECT
         block_height,
-        collection_count
+        collection_count,
+        ARRAY_AGG(
+            VALUE :collection_id :: STRING
+        ) AS collections_expected
     FROM
-        {{ ref('silver__streamline_blocks') }}
-
-        {% if var(
+        {{ ref('silver__streamline_blocks') }},
+        LATERAL FLATTEN(collection_guarantees) {% if var(
                 'TEST_RANGE',
                 False
             ) %}
@@ -19,13 +21,17 @@ WITH collections_expected AS (
             block_height BETWEEN {{ var('start_height') }}
             AND {{ var('end_height') }}
         {% endif %}
+    GROUP BY
+        1,
+        2
 ),
 collections_actual AS (
     SELECT
         block_number AS block_height,
         COUNT(
             DISTINCT collection_id
-        ) AS collection_count
+        ) AS collection_count,
+        ARRAY_AGG(collection_id) AS collections_actual
     FROM
         {{ ref('silver__streamline_collections') }}
 
@@ -43,10 +49,16 @@ collections_actual AS (
 SELECT
     e.block_height,
     e.collection_count AS expected,
-    a.collection_count AS actual,
-    e.collection_count - a.collection_count AS difference
+    A.collection_count AS actual,
+    e.collection_count - A.collection_count AS difference,
+    e.collections_expected,
+    A.collections_actual,
+    silver.array_disjunctive_union(
+        e.collections_expected,
+        A.collections_actual
+    ) AS missing_collections
 FROM
     collections_expected e
-    JOIN collections_actual a USING(block_height)
+    JOIN collections_actual A USING(block_height)
 WHERE
-    e.collection_count != a.collection_count
+    e.collection_count != A.collection_count
