39b3294c9297f545dc8b5e6748ef6ea43b8e1aa4
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-10-11 16:28:52 -0600

    defi schema

--- models/gold/defi/defi__ez_bridge_transactions.sql
+++ models/gold/defi/defi__ez_bridge_transactions.sql
@@ -1,27 +1,49 @@
 {{ config(
     materialized = 'view',
     tags = ['ez', 'bridge', 'scheduled'],
-    meta={
-    'database_tags':{
-        'table': {
-            'PURPOSE': 'BRIDGE'
-            }
-        }
-    }
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'BRIDGE' }} }
 ) }}
 
-WITH blocto AS (
+WITH blocto_cw AS (
 
     SELECT
         *
     FROM
         {{ ref('silver__bridge_blocto') }}
+    WHERE
+        block_height < {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
 ),
-celer AS (
+celer_cw AS (
     SELECT
         *
     FROM
         {{ ref('silver__bridge_celer') }}
+    WHERE
+        block_height < {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
+),
+blocto_s AS (
+    SELECT
+        *
+    FROM
+        {{ ref('silver__bridge_blocto_s') }}
+    WHERE
+        block_height >= {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
+),
+celer_s AS (
+    SELECT
+        *
+    FROM
+        {{ ref('silver__bridge_celer_s') }}
+    WHERE
+        block_height >= {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
 ),
 combo AS (
     SELECT
@@ -36,7 +58,7 @@ combo AS (
         teleport_direction AS direction,
         bridge
     FROM
-        blocto
+        blocto_cw
     UNION
     SELECT
         tx_id,
@@ -50,7 +72,35 @@ combo AS (
         direction,
         bridge
     FROM
-        celer
+        celer_cw
+    UNION
+    SELECT
+        tx_id,
+        block_timestamp,
+        block_height,
+        teleport_contract AS bridge_contract,
+        token_contract,
+        gross_amount AS amount,
+        flow_wallet_address,
+        blockchain,
+        teleport_direction AS direction,
+        bridge
+    FROM
+        blocto_s
+    UNION
+    SELECT
+        tx_id,
+        block_timestamp,
+        block_height,
+        bridge_contract,
+        token_contract,
+        amount,
+        flow_wallet_address,
+        blockchain,
+        direction,
+        bridge
+    FROM
+        celer_s
 )
 SELECT
     *
--- models/gold/defi/defi__ez_swaps.sql
+++ models/gold/defi/defi__ez_swaps.sql
@@ -1,27 +1,64 @@
 {{ config (
     materialized = 'view',
     tags = ['ez', 'scheduled'],
-    meta={
-    'database_tags':{
-        'table': {
-            'PURPOSE': 'SWAPS'
-            }
-        }
-    }
+    meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'SWAPS' }} }
 ) }}
 
+WITH chainwalkers AS (
+
+    SELECT
+        tx_id,
+        block_timestamp,
+        block_height,
+        swap_contract,
+        swap_index,
+        trader,
+        token_out_source,
+        token_out_contract,
+        token_out_amount,
+        token_in_destination,
+        token_in_contract,
+        token_in_amount
+    FROM
+        {{ ref('silver__swaps') }}
+    WHERE
+        block_height < {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
+),
+streamline AS (
+    SELECT
+        tx_id,
+        block_timestamp,
+        block_height,
+        swap_contract,
+        swap_index,
+        trader,
+        token_out_source,
+        token_out_contract,
+        token_out_amount,
+        token_in_destination,
+        token_in_contract,
+        token_in_amount
+    FROM
+        {{ ref('silver__swaps_s') }}
+    WHERE
+        block_height >= {{ var(
+            'STREAMLINE_START_BLOCK'
+        ) }}
+),
+FINAL AS (
+    SELECT
+        *
+    FROM
+        chainwalkers
+    UNION
+    SELECT
+        *
+    FROM
+        streamline
+)
 SELECT
-    tx_id,
-    block_timestamp,
-    block_height,
-    swap_contract,
-    swap_index,
-    trader,
-    token_out_source,
-    token_out_contract,
-    token_out_amount,
-    token_in_destination,
-    token_in_contract,
-    token_in_amount
+    *
 FROM
-    {{ ref('silver__swaps') }}
+    FINAL
