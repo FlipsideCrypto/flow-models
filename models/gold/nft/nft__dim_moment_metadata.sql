48e8d4c24cfb00a2f988d8f498c969019627ad43
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-10-11 17:20:15 -0600

    nft schema

--- models/gold/nft/nft__dim_moment_metadata.sql
+++ models/gold/nft/nft__dim_moment_metadata.sql
@@ -10,27 +10,65 @@
     }
 ) }}
 
+WITH chainwalkers AS (
+
+    SELECT
+        event_contract AS nft_collection,
+        nft_id,
+        serial_number,
+        max_mint_size,
+        play_id,
+        series_id,
+        series_name,
+        set_id,
+        set_name,
+        edition_id,
+        tier,
+        metadata
+    FROM
+        {{ ref('silver__nft_moment_metadata_final') }}
+    WHERE
+        NOT (
+            nft_collection = 'A.87ca73a41bb50ad5.Golazos'
+            AND edition_id = 486
+        )
+        AND NOT (
+            nft_collection = 'A.e4cf4bdc1751c65d.AllDay'
+            AND edition_id = 1486
+        )
+),
+streamline AS (
+    SELECT
+        event_contract AS nft_collection,
+        nft_id,
+        serial_number,
+        max_mint_size,
+        play_id,
+        series_id,
+        series_name,
+        set_id,
+        set_name,
+        edition_id,
+        tier,
+        metadata
+    FROM
+        {{ ref('silver__nft_moment_metadata_final_s') }}
+    WHERE
+        NOT (
+            nft_collection = 'A.87ca73a41bb50ad5.Golazos'
+            AND edition_id = 486
+        )
+        AND NOT (
+            nft_collection = 'A.e4cf4bdc1751c65d.AllDay'
+            AND edition_id = 1486
+        )
+)
 SELECT
-    event_contract AS nft_collection,
-    nft_id,
-    serial_number,
-    max_mint_size,
-    play_id,
-    series_id,
-    series_name,
-    set_id,
-    set_name,
-    edition_id,
-    tier,
-    metadata
+    *
 FROM
-    {{ ref('silver__nft_moment_metadata_final') }}
-WHERE
-    NOT (
-        nft_collection = 'A.87ca73a41bb50ad5.Golazos'
-        AND edition_id = 486
-    )
-    AND NOT (
-        nft_collection = 'A.e4cf4bdc1751c65d.AllDay'
-        AND edition_id = 1486
-    )
+    chainwalkers
+UNION
+SELECT
+    *
+FROM
+    streamline
