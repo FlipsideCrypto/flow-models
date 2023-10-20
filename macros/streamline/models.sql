0d4c7df261da565a0b9a330b8b2c06b0c7299a66
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-10-20 11:30:01 -0600

    multiple external tbl query macro and blocks update

--- macros/streamline/models.sql
+++ macros/streamline/models.sql
@@ -85,3 +85,68 @@
 {% endmacro %}
 
 
+    {% macro streamline_multiple_external_table_query(
+        table_names,
+        partition_function,
+        partition_name,
+        unique_key
+    )%}
+    WITH 
+    {% for table_name in table_names %}
+        meta_{{ table_name }} AS (
+            SELECT
+                last_modified AS _inserted_timestamp,
+                file_name,
+                {{ partition_function }} AS {{ partition_name }}
+            FROM
+                TABLE(
+                    information_schema.external_table_file_registration_history(
+                        start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
+                        table_name => '{{ source( "bronze_streamline", table_name ) }}')
+                    ) A
+            ),
+            {{ table_name }} AS (
+                SELECT
+                    block_number,
+                    {{ unique_key }},
+                    DATA,
+                    _inserted_timestamp,
+                    MD5(
+                        CAST(
+                            COALESCE(CAST(block_number AS text), '' :: STRING) AS text
+                        )
+                    ) AS _fsc_id,
+                    s.{{ partition_name }},
+                    s.value AS VALUE
+                FROM
+                    {{ source(
+                        "bronze_streamline",
+                        table_name
+                    ) }}
+                    s
+                    JOIN meta_{{ table_name }}
+                    b
+                    ON b.file_name = metadata$filename
+                    AND b.{{ partition_name }} = s.{{ partition_name }}
+                WHERE
+                    b.{{ partition_name }} = s.{{ partition_name }}
+            ),
+    {% endfor %}
+
+    FINAL AS ({% for table_name in table_names %}
+        SELECT
+            *
+        FROM
+            {{ table_name }}
+
+        {% if not loop.last %}
+                UNION ALL
+        {% endif %}
+        {% endfor %}
+    )
+    SELECT
+        *
+    FROM
+        FINAL
+
+    {% endmacro %}
--- models/silver/streamline/bronze/core/bronze__streamline_blocks.sql
+++ models/silver/streamline/bronze/core/bronze__streamline_blocks.sql
@@ -7,59 +7,10 @@ table_names = [
     'BLOCKS_CANDIDATE_07', 'BLOCKS_CANDIDATE_08', 'BLOCKS_CANDIDATE_09', 'BLOCKS_MAINNET_01', 'BLOCKS_MAINNET_02', 'BLOCKS_MAINNET_03', 'BLOCKS_MAINNET_04', 'BLOCKS_MAINNET_05', 'BLOCKS_MAINNET_06', 'BLOCKS_MAINNET_07', 'BLOCKS_MAINNET_08', 'BLOCKS_MAINNET_09', 'BLOCKS_MAINNET_10', 'BLOCKS_MAINNET_11', 'BLOCKS_MAINNET_12', 'BLOCKS_MAINNET_13', 'BLOCKS_MAINNET_14', 'BLOCKS_MAINNET_15', 'BLOCKS_MAINNET_16', 'BLOCKS_MAINNET_17', 'BLOCKS_MAINNET_18', 'BLOCKS_MAINNET_19', 'BLOCKS_MAINNET_20', 'BLOCKS_MAINNET_21', 'BLOCKS_MAINNET_22'
 ]
 %}
-WITH 
-{% for table_name in table_names %}
-    meta_{{ table_name }} AS (
-        SELECT
-            last_modified AS _inserted_timestamp,
-            file_name,
-            CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER) AS _partition_by_block_id
-        FROM
-            TABLE(
-                information_schema.external_table_file_registration_history(
-                    start_time => DATEADD('day', -3, CURRENT_TIMESTAMP()),
-                    table_name => '{{ source( "bronze_streamline", table_name ) }}')
-                ) A
-        ),
-        {{ table_name }} AS (
-            SELECT
-                block_number,
-                DATA,
-                _inserted_timestamp,
-                MD5(
-                    CAST(
-                        COALESCE(CAST(block_number AS text), '' :: STRING) AS text
-                    )
-                ) AS _fsc_id,
-                s._partition_by_block_id,
-                s.value AS VALUE
-            FROM
-                {{ source(
-                    "bronze_streamline",
-                    table_name
-                ) }}
-                s
-                JOIN meta_{{ table_name }}
-                b
-                ON b.file_name = metadata$filename
-                AND b._partition_by_block_id = s._partition_by_block_id
-            WHERE
-                b._partition_by_block_id = s._partition_by_block_id
-        ),
-{% endfor %}
 
-FINAL AS ({% for table_name in table_names %}
-    SELECT
-        *
-    FROM
-        {{ table_name }}
-
-    {% if not loop.last %}
-            UNION ALL
-    {% endif %}
-    {% endfor %}
-)
-SELECT
-    *
-FROM
-    FINAL
+{{ streamline_multiple_external_table_query(
+    table_names,
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
+    partition_name = "_partition_by_block_id",
+    unique_key = "id"
+) }}
