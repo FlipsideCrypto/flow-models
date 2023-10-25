7d814c27ab3fff4d0eef1823e346c1ab6843a255
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-10-25 11:30:30 -0600

    collections template fix

--- python/create_views/input/streamline__get_blocks_history_NETWORK_VERSION.sql
+++ python/create_views/input/streamline__get_blocks_history_NETWORK_VERSION.sql
@@ -3,7 +3,8 @@
     post_hook = if_data_call_function(
         func = "{{this.schema}}.udf_bulk_grpc_us_east_2(object_construct('sql_source', '{{this.identifier}}','node_url','NETWORK_NODE_URL','external_table', 'blocks_NETWORK_VERSION', 'sql_limit', {{var('sql_limit','500000')}}, 'producer_batch_size', {{var('producer_batch_size','10000')}}, 'worker_batch_size', {{var('worker_batch_size','1000')}}, 'batch_call_limit', {{var('batch_call_limit','1')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
-    )
+    ),
+    tags = ['streamline_history']
 ) }}
 
 WITH blocks AS (
--- python/create_views/input/streamline__get_collections_history_NETWORK_VERSION.sql
+++ python/create_views/input/streamline__get_collections_history_NETWORK_VERSION.sql
@@ -6,38 +6,22 @@
     )
 ) }}
 
-WITH blocks AS (
-
-    SELECT
-        block_height
-    FROM
-        {{ ref("streamline__blocks") }}
-    EXCEPT
-    SELECT
-        block_number AS block_height
-    FROM
-        {{ ref("streamline__complete_get_collections") }}
-),
-collections AS (
-    SELECT
-        block_number AS block_height,
-        DATA
-    FROM
-        {{ ref('streamline__complete_get_blocks') }}
-        JOIN blocks
-        ON blocks.block_height = block_number
-),
--- CTE to get all block_heights and their associated collection_ids from the complete_get_blocks table
+WITH 
+-- CTE to get all block_heights and their associated collection_ids from the complete_get_blocks_history table
 block_collections AS (
+
     SELECT
         cb.block_number AS block_height,
         collection_guarantee.value :collection_id AS collection_id
     FROM
-        {{ ref("streamline__complete_get_blocks") }}
+        {{ ref("streamline__complete_get_blocks_history") }}
         cb,
         LATERAL FLATTEN(
             input => cb.data :collection_guarantees
         ) AS collection_guarantee
+    WHERE
+        block_height BETWEEN root_height
+        AND end_height
 ),
 -- CTE to identify collections that haven't been ingested yet
 collections_to_ingest AS (
@@ -46,27 +30,32 @@ collections_to_ingest AS (
         bc.collection_id
     FROM
         block_collections bc
-        LEFT JOIN {{ ref("streamline__complete_get_collections") }} C
+        LEFT JOIN {{ ref("streamline__complete_get_collections_history") }} C
         ON bc.block_height = C.block_number
         AND bc.collection_id = C.id
     WHERE
         C.id IS NULL
-) -- Generate the requests based on the missing collections
+        AND (
+            block_height BETWEEN root_height
+            AND end_height
+        )
+) 
+-- Generate the requests based on the missing collections
 SELECT
     OBJECT_CONSTRUCT(
-        'grpc', 'proto3',
-        'method', 'get_collection_by_i_d',
-        'block_height', block_height :: INTEGER,
+        'grpc',
+        'proto3',
+        'method',
+        'get_collection_by_i_d',
+        'block_height',
+        block_height :: INTEGER,
         'method_params',
-            OBJECT_CONSTRUCT(
-                'id',
-                collection_id
-            )
+        OBJECT_CONSTRUCT(
+            'id',
+            collection_id
+        )
     ) AS request
 FROM
     collections_to_ingest
-WHERE
-    block_height BETWEEN root_height
-    AND end_height
 ORDER BY
     block_height ASC
