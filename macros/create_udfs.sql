d17d5f06d70346b31f2b9516cb3a96eebfea53c0
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2023-11-07 18:40:24 +0100

    deprecation (#222)
    * deprecation
    
    * del old udf creation
    
    ---------
    
    Co-authored-by: forgash_ <jmfxyz@pm.me>

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,9 +2,6 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% if target.database != "FLOW_COMMUNITY_DEV" %}
             {% set sql %}
-            
-            {{ udf_bulk_get_topshot_moments_minted_metadata() }};
-            {{ udf_bulk_get_nfl_allday_moments_metadata() }};
 
             {{ create_udtf_get_base_table(
                 schema = "streamline"
