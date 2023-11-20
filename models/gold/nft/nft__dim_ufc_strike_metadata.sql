5bd88cc150f02196583038d7cea398ade65048c2
forgash_ <jmfxyz@pm.me>
2023-11-20 15:43:17 -0700

    ufc strike metadata, plus organized nft folder better

---
+++ models/gold/nft/nft__dim_ufc_strike_metadata.sql
@@ -0,0 +1,23 @@
+{{ config(
+    materialized = 'view',
+    meta={
+    'database_tags':{
+        'table': {
+            'PURPOSE': 'NFT, TOPSHOT'
+            }
+        }
+    },
+    tags = ['scheduled_non_core']
+) }}
+
+SELECT
+    nft_id,
+    edition_id,
+    listing_id,
+    set_name,
+    metadata,
+    inserted_timestamp,
+    modified_timestamp,
+    invocation_id
+FROM
+    {{ ref('silver__nft_ufc_strike_metadata') }}
---
+++ models/silver/nft/metadata/silver__nft_ufc_strike_metadata.sql
@@ -0,0 +1,49 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['nft_id'],
+    merge_exclude_columns = ["inserted_timestamp"],
+    tags = ['scheduled_non_core']
+) }}
+
+WITH metadata AS (
+
+    SELECT
+        token_id,
+        edition,
+        owner,
+        listing_id,
+        set_name,
+        metadata,
+        _inserted_timestamp,
+        _filename
+    FROM
+        {{ source(
+            'flow_bronze',
+            'ufc_strike_metadata'
+        ) }}
+
+{% if is_incremental %}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
+{% endif %}
+)
+SELECT
+    token_id AS nft_id,
+    edition AS edition_id,
+    owner,
+    listing_id,
+    set_name,
+    metadata,
+    _inserted_timestamp,
+    _filename,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{invocation_id}}' AS invocation_id
+FROM
+    metadata
