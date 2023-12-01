0d03243c71d7c4318fbd404ce4724c2e966fd0cb
WHYTEWYLL <juanmiblanco1@gmail.com>
2023-12-01 12:24:04 -0300

    delete: tags

--- tests/tests__bronze_api_metadata.sql
+++ tests/tests__bronze_api_metadata.sql
@@ -1,45 +1,55 @@
 {{ config(
     severity = "warn",
     error_if = ">100",
-    tags = ['metadata']
 ) }}
 
-
-
 WITH latest_day AS (
+
     SELECT
         DATE_TRUNC('day', MAX(_inserted_timestamp)) AS last_ingestion_day
     FROM
-        {{ source('bronze_api','allday_metadata') }}        
-        )
-, api_call AS (
+        {{ source(
+            'bronze_api',
+            'allday_metadata'
+        ) }}
+),
+api_call AS (
     SELECT
         *
     FROM
-        flow.bronze_api.allday_metadata  
+        flow.bronze_api.allday_metadata
     WHERE
-    _inserted_timestamp::date =  (SELECT last_ingestion_day FROM latest_day)
-    AND contract = 'A.e4cf4bdc1751c65d.AllDay'
+        _inserted_timestamp :: DATE = (
+            SELECT
+                last_ingestion_day
+            FROM
+                latest_day
+        )
+        AND contract = 'A.e4cf4bdc1751c65d.AllDay'
 ),
-FLATTEN_RES AS (
+flatten_res AS (
     SELECT
-        REQUESTED_IDS,
+        requested_ids,
         ARRAY_SIZE(requested_ids) AS requested_ids_length,
-        ARRAY_SIZE(res:data:data:searchMomentNFTsV2:edges) AS res_length,
+        ARRAY_SIZE(
+            res :data :data :searchMomentNFTsV2 :edges
+        ) AS res_length,
         _INSERTED_TIMESTAMP
     FROM
         api_call
 ),
 mismatch_calc AS (
     SELECT
-        REQUESTED_IDS,
+        requested_ids,
         CASE
             WHEN requested_ids_length = 0 THEN 0
-            ELSE ABS(requested_ids_length - res_length) / requested_ids_length * 100
+            ELSE ABS(
+                requested_ids_length - res_length
+            ) / requested_ids_length * 100
         END AS mismatch_percentage,
         _INSERTED_TIMESTAMP
     FROM
-        FLATTEN_RES
+        flatten_res
 )
 SELECT
     *
