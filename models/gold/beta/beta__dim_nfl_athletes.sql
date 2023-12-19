40afac7a554783d70f51eb76205895fa958e34d2
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2023-12-19 13:08:30 -0500

    list cols

--- models/gold/beta/beta__dim_nfl_athletes.sql
+++ models/gold/beta/beta__dim_nfl_athletes.sql
@@ -3,7 +3,42 @@
 ) }}
 
 SELECT
-    *
+    ref,
+    id,
+    UID,
+    guid,
+    TYPE,
+    alternate_ids,
+    first_name,
+    last_name,
+    full_name,
+    display_name,
+    short_name,
+    weight,
+    display_weight,
+    height,
+    display_height,
+    age,
+    date_of_birth,
+    links,
+    birth_place,
+    college,
+    slug,
+    headshot,
+    jersey,
+    POSITION,
+    linked,
+    team,
+    statistics,
+    contracts,
+    experience,
+    college_athlete,
+    active,
+    draft,
+    status,
+    statisticslog,
+    debut_year,
+    _inserted_timestamp :: timestamp_ntz AS inserted_timestamp
 FROM
     {{ source(
         'flow_bronze',
