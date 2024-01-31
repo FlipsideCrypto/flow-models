9fd73c07f43622b11cba954875712da0594bff41
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-01-31 15:24:31 +0100

    delete coalesces

--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -11,13 +11,8 @@ SELECT
     label_type,
     label_subtype,
     project_name AS label,
-    COALESCE(
-        labels_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['address']
-        ) }}
-    ) AS dim_labels_id,
-    COALESCE(inserted_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) as inserted_timestamp,
-    COALESCE(modified_timestamp, '2000-01-01' :: TIMESTAMP_NTZ) as modified_timestamp
+    labels_id AS dim_labels_id,
+    inserted_timestamp,
+    modified_timestamp
 FROM
     {{ ref('silver__labels') }}
