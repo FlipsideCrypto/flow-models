4f44625fd3d3ff5a1c7db51b0ce6df82df6d4514
WHYTEWYLL <49686519+WHYTEWYLL@users.noreply.github.com>
2024-02-15 15:27:07 +0100

    increasing producer_batch_size

--- models/silver/streamline/core/history/transaction_results/streamline__get_transaction_results_history_mainnet_18.sql
+++ models/silver/streamline/core/history/transaction_results/streamline__get_transaction_results_history_mainnet_18.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_grpc_us_east_2(object_construct('sql_source', '{{this.identifier}}','node_url','access-001.mainnet18.nodes.onflow.org:9000','external_table', 'transaction_results_mainnet_18', 'sql_limit', '500000', 'producer_batch_size', '1250', 'worker_batch_size', '1', 'batch_call_limit', {{var('batch_call_limit','1')}}))",
+        func = "{{this.schema}}.udf_bulk_grpc_us_east_2(object_construct('sql_source', '{{this.identifier}}','node_url','access-001.mainnet18.nodes.onflow.org:9000','external_table', 'transaction_results_mainnet_18', 'sql_limit', '500000', 'producer_batch_size', '2500', 'worker_batch_size', '1', 'batch_call_limit', {{var('batch_call_limit','1')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
