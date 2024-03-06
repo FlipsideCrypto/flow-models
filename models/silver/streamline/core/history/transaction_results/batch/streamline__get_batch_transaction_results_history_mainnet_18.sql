fcdc144211885673c21b3348a2ec7072cde13e8b
shah <info@shahnewazkhan.ca>
2024-03-06 15:50:00 -0800

    WIP throughput tune

--- models/silver/streamline/core/history/transaction_results/batch/streamline__get_batch_transaction_results_history_mainnet_18.sql
+++ models/silver/streamline/core/history/transaction_results/batch/streamline__get_batch_transaction_results_history_mainnet_18.sql
@@ -6,9 +6,9 @@
         params = {
             "node_url":"access-001.mainnet18.nodes.onflow.org:9000",
             "external_table": "transaction_results_batch_mainnet_18",
-            "sql_limit": "100",
-            "producer_batch_size": "100",
-            "worker_batch_size": "100",
+            "sql_limit": "72000",
+            "producer_batch_size": "4000",
+            "worker_batch_size": "1000",
             "sql_source": "{{this.identifier}}"
         }
     )
