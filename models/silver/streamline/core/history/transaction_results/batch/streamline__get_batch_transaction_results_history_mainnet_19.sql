bc7c5a4c6a9f616df5f00aa1c5d293bf062bba77
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-03-19 09:13:13 -0600

    decr nv 19

--- models/silver/streamline/core/history/transaction_results/batch/streamline__get_batch_transaction_results_history_mainnet_19.sql
+++ models/silver/streamline/core/history/transaction_results/batch/streamline__get_batch_transaction_results_history_mainnet_19.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "streamline.udf_bulk_grpc(object_construct('node_url','access-001.mainnet19.nodes.onflow.org:9000', 'external_table', 'transaction_results_mainnet_19', 'sql_limit', '750000', 'producer_batch_size', '50000', 'worker_batch_size', '500', 'sql_source', '{{this.identifier}}', 'concurrent_requests', '750'))",
+        func = "streamline.udf_bulk_grpc(object_construct('node_url','access-001.mainnet19.nodes.onflow.org:9000', 'external_table', 'transaction_results_mainnet_19', 'sql_limit', '700000', 'producer_batch_size', '35000', 'worker_batch_size', '350', 'sql_source', '{{this.identifier}}', 'concurrent_requests', '750'))",
         target = "streamline.{{this.identifier}}"
         )        
     )
