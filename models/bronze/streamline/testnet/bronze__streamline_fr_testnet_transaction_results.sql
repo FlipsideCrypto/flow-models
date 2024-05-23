c6f06d6773792f09db4a49e6e630e8dc85552a42
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-05-23 16:37:25 -0600

    upd model in bronze views

--- models/bronze/streamline/testnet/bronze__streamline_fr_testnet_transaction_results.sql
+++ models/bronze/streamline/testnet/bronze__streamline_fr_testnet_transaction_results.sql
@@ -2,7 +2,7 @@
     materialized = 'view'
 ) }}
 
-{% set model = this.identifier.split("_")[-2:] | join('_') %}
+{% set model = this.identifier.split("_")[-3:] | join('_') %}
 {{ streamline_external_table_FR_query(
     model,
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
--- models/bronze/streamline/testnet/bronze__streamline_testnet_transactions.sql
+++ models/bronze/streamline/testnet/bronze__streamline_testnet_transactions.sql
@@ -2,7 +2,7 @@
     materialized = 'view'
 ) }}
 
-{% set model = this.identifier.split("_") [-1] %}
+{% set model = this.identifier.split("_")[-2:] | join('_') %}
 {{ streamline_external_table_query(
     model,
     partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
