788e7ffa64765c9c6be785423c09f862031d7f29
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-06-05 13:43:08 -0600

    blocks backfill logic ez ez ez

--- models/bronze/streamline/history/bronze__streamline_blocks_history.sql
+++ models/bronze/streamline/history/bronze__streamline_blocks_history.sql
@@ -2,15 +2,35 @@
     materialized = 'view'
 ) }}
 
-{% set 
-table_names = [
-    'BLOCKS_CANDIDATE_07', 'BLOCKS_CANDIDATE_08', 'BLOCKS_CANDIDATE_09', 'BLOCKS_MAINNET_01', 'BLOCKS_MAINNET_02', 'BLOCKS_MAINNET_03', 'BLOCKS_MAINNET_04', 'BLOCKS_MAINNET_05', 'BLOCKS_MAINNET_06', 'BLOCKS_MAINNET_07', 'BLOCKS_MAINNET_08', 'BLOCKS_MAINNET_09', 'BLOCKS_MAINNET_10', 'BLOCKS_MAINNET_11', 'BLOCKS_MAINNET_12', 'BLOCKS_MAINNET_13', 'BLOCKS_MAINNET_14', 'BLOCKS_MAINNET_15', 'BLOCKS_MAINNET_16', 'BLOCKS_MAINNET_17', 'BLOCKS_MAINNET_18', 'BLOCKS_MAINNET_19', 'BLOCKS_MAINNET_20', 'BLOCKS_MAINNET_21', 'BLOCKS_MAINNET_22'
-]
-%}
+{% set history_model = "BLOCKS_" ~ var('LOAD_BACKFILL_VERSION', Null) %}
 
-{{ streamline_multiple_external_table_query(
-    table_names,
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
-    partition_name = "_partition_by_block_id",
-    unique_key = "id"
-) }}
+WITH meta AS (
+    SELECT
+        registered_on AS _inserted_timestamp,
+        file_name,
+        CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER) AS _partition_by_block_id
+    FROM
+        TABLE(
+            information_schema.external_table_files(
+                table_name => '{{ source( "bronze_streamline", history_model ) }}'
+            )
+        ) A
+)
+SELECT
+    block_number,
+    DATA,
+    _inserted_timestamp,
+    MD5(
+        CAST(
+            COALESCE(CAST(block_number AS text), '' :: STRING) AS text
+        )
+    ) AS _fsc_id,
+    s._partition_by_block_id,
+    s.value AS VALUE
+FROM
+    {{ source("bronze_streamline", history_model ) }} s
+    JOIN meta b
+    ON b.file_name = metadata$filename
+    AND b._partition_by_block_id = s._partition_by_block_id
+WHERE
+    b._partition_by_block_id = s._partition_by_block_id
