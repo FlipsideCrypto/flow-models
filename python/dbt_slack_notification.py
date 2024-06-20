04860f8e8a4204162b5dfda371f407b294ab21a7
forgash_ <jmfxyz@pm.me>
2024-06-20 10:04:42 -0600

    print data

--- python/dbt_slack_notification.py
+++ python/dbt_slack_notification.py
@@ -69,5 +69,7 @@ if __name__ == '__main__':
         "dbt_run_status": os.environ.get("DBT_RUN_STATUS")
     }
 
+    print(data)
+
     webhook_url = os.environ.get("SLACK_WEBHOOK_URL")
     send_alert(webhook_url, data)
