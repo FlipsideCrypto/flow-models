c71e6f08ed8200bafdf2bff5829bba04d905ab34
forgash_ <jmfxyz@pm.me>
2024-06-20 10:17:13 -0600

    add emoji to status

--- python/dbt_slack_notification.py
+++ python/dbt_slack_notification.py
@@ -28,7 +28,7 @@ def create_message(**kwargs):
                     },
                     {
                         "title": "Run status",
-                        "value": kwargs["dbt_run_status"],
+                        "value": f"{':check:' if kwargs["dbt_run_status"] == 'success' else ':x:'} {kwargs['dbt_run_status'].capitalize()}",
                         "short": True
                     },
                     {
