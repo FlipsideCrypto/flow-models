0e20e70a3669e41f7bf89f6c1c8915866f8b0672
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-07-30 13:32:56 -0600

    depr notice on ez. Tests on fact bridge. TODOs - naming convention for amount and use chain IDs??

--- models/gold/defi/defi__fact_bridge_transactions.sql
+++ models/gold/defi/defi__fact_bridge_transactions.sql
@@ -4,70 +4,22 @@
     meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'BRIDGE' }} }
 ) }}
 
-WITH
-blocto_s AS (
-    SELECT
-        *
-    FROM
-        {{ ref('silver__bridge_blocto_s') }}
-),
-celer_s AS (
-    SELECT
-        *
-    FROM
-        {{ ref('silver__bridge_celer_s') }}
-),
-combo AS (
-    SELECT
-        bridge_blocto_id AS bridge_id,
-        tx_id,
-        block_timestamp,
-        block_height,
-        teleport_contract AS bridge_contract,
-        token_contract,
-        gross_amount AS amount,
-        flow_wallet_address,
-        blockchain,
-        teleport_direction AS direction,
-        bridge,
-        inserted_timestamp,
-        modified_timestamp
-    FROM
-        blocto_s
-    UNION ALL
-    SELECT
-        bridge_celer_id AS bridge_id,
-        tx_id,
-        block_timestamp,
-        block_height,
-        bridge_contract,
-        token_contract,
-        amount,
-        flow_wallet_address,
-        blockchain,
-        direction,
-        bridge,
-        inserted_timestamp,
-        modified_timestamp
-    FROM
-        celer_s
-)
 SELECT
     tx_id,
     block_timestamp,
     block_height,
-    bridge_contract,
-    token_contract AS token_address,
-    amount, -- adj or unadj?
-    flow_wallet_address,
-    blockchain,
-    direction,
-    bridge,
-    COALESCE (
-        bridge_id,
-        {{ dbt_utils.generate_surrogate_key(['tx_id']) }}
-    ) AS ez_bridge_transactions_id,
+    bridge_address,
+    token_address,
+    gross_amount,
+    amount_fee,
+    net_amount,
+    source_address,
+    destination_address,
+    source_chain,
+    destination_chain,
+    platform,
+    bridge_complete_id AS fact_bridge_transactions_id,
     inserted_timestamp,
     modified_timestamp
 FROM
-    combo
+    {{ ref('silver__bridge_complete') }}
