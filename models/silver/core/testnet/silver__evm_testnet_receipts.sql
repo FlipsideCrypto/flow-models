a268c8802f7621f1b64ecb2dbcc884b7998ab386
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-08-22 22:30:21 -0600

    receipts silver

---
+++ models/silver/core/testnet/silver__evm_testnet_receipts.sql
@@ -0,0 +1,63 @@
+-- depends_on: {{ ref('bronze__streamline_evm_testnet_receipts') }}
+-- depends_on: {{ ref('bronze__streamline_fr_evm_testnet_receipts') }}
+{{ config(
+    materialized = 'incremental',
+    unique_key = "evm_testnet_receipts_id",
+    incremental_strategy = 'merge',
+    merge_exclude_columns = ["inserted_timestamp"],
+    cluster_by = ['_inserted_timestamp :: DATE', 'block_number'],
+    tags = ['evm_testnet', 'crescendo']
+) }}
+
+WITH receipts AS (
+
+    SELECT
+        block_number,
+        DATA,
+        _partition_by_block_id,
+        _inserted_timestamp
+    FROM
+
+{% if is_incremental() %}
+{{ ref('bronze__streamline_evm_testnet_receipts') }}
+WHERE
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp) _inserted_timestamp
+        FROM
+            {{ this }}
+    )
+{% else %}
+    {{ ref('bronze__streamline_fr_evm_testnet_receipts') }}
+{% endif %}
+
+)
+SELECT
+    block_number,
+    VALUE :blobGasPrice :: INT AS blob_gas_price,
+    VALUE :blockHash :: STRING AS block_hash,
+    VALUE :contractAddress :: STRING AS contract_address,
+    VALUE :cumulativeGasUsed :: INT AS cumulative_gas_used,
+    VALUE :effectiveGasPrice :: INT AS effective_gas_price,
+    VALUE :logs :: ARRAY AS logs,
+    VALUE :logsBloom :: STRING AS logs_bloom,
+    VALUE :revertReason :: STRING AS revert_reason,
+    VALUE :status :: INT AS tx_status,
+    VALUE :transactionHash :: STRING AS tx_hash,
+    VALUE :transactionIndex :: INT AS tx_index,
+    VALUE :type :: STRING AS tx_type,
+    _partition_by_block_id,
+    _inserted_timestamp,
+    {{ dbt_utils.generate_surrogate_key(
+        ['block_number', 'VALUE:transactionHash::STRING']
+    ) }} AS evm_testnet_receipts_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp,
+    '{{ invocation_id }}' AS _invocation_id
+FROM
+    receipts,
+    LATERAL FLATTEN (DATA :result :: variant)
+
+qualify(ROW_NUMBER() over (PARTITION BY evm_testnet_receipts_id
+ORDER BY
+    _inserted_timestamp DESC)) = 1
