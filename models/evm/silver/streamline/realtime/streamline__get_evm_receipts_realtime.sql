4f286982c57f75f6d179e3bb09c0cf21579b3230
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-09-05 20:39:54 -0600

    incr batch limit due to late start

--- models/evm/silver/streamline/realtime/streamline__get_evm_receipts_realtime.sql
+++ models/evm/silver/streamline/realtime/streamline__get_evm_receipts_realtime.sql
@@ -4,9 +4,9 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"evm_receipts",
-        "sql_limit" :"10000",
-        "producer_batch_size" :"2000",
-        "worker_batch_size" :"500",
+        "sql_limit" :"25000",
+        "producer_batch_size" :"5000",
+        "worker_batch_size" :"1000",
         "sql_source" :"{{this.identifier}}" }
     ),
     tags = ['streamline_realtime_evm']
