0cdd32464ac4c5a8364da171833bae65e17e49d2
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-09-25 14:11:52 -0400

    add schema tag for flow evm

--- macros/tags/add_database_or_schema_tags.sql
+++ macros/tags/add_database_or_schema_tags.sql
@@ -1,3 +1,11 @@
 {% macro add_database_or_schema_tags() %}
-    {{ set_database_tag_value('BLOCKCHAIN_NAME','FLOW') }}
+    {{ set_database_tag_value(
+        'BLOCKCHAIN_NAME',
+        'FLOW'
+    ) }}
+    {{ set_schema_tag_value(
+        'CORE_EVM',
+        'BLOCKCHAIN_NAME',
+        'FLOW-EVM'
+    ) }}
 {% endmacro %}
