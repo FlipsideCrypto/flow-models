64a009f0ab5f449dc9fe72047934b7e566d2d563
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2024-10-11 14:16:25 -0600

    AN-5252/points api integration (#368)
    * basic query
    
    * external points api request
    
    * rn REWARD_POINTS and add supplementary models
    
    * upd vault
    
    * add gha workflows
    
    * add headers incl user-agent
    
    * upd header
    
    * rm content type header
    
    * addback secret
    
    * use target
    
    * rn _realtime
    
    * upd silver and add FR view

---
+++ models/silver/streamline/external/streamline__reward_points_realtime.sql
@@ -0,0 +1,39 @@
+{{ config (
+    materialized = "view",
+    post_hook = fsc_utils.if_data_call_function_v2(
+        func = '{{this.schema}}.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params = {
+            "external_table": "reward_points",
+            "sql_limit": "1000",
+            "producer_batch_size": "1000",
+            "worker_batch_size": "1000",
+            "sql_source": "{{this.identifier}}"
+        }
+    ),
+    tags = ['streamline_non_core']
+) }}
+
+WITH evm_addresses AS (
+
+    SELECT
+        DISTINCT address AS address
+    FROM
+        {{ ref('streamline__evm_addresses') }}
+)
+SELECT
+    DATE_PART('EPOCH', SYSDATE()) :: INTEGER AS partition_key,
+    address,
+    {{ target.database }}.live.udf_api(
+        'GET',
+        '{Service}/points/ethereum/' || address,
+        {
+            'User-Agent': 'Flipside/0.1',
+            'Accept': 'application/json',
+            'Connection': 'keep-alive'
+        },
+        {},
+        'Vault/prod/flow/points-api/prod'
+    ) AS request
+FROM
+    evm_addresses
