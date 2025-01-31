7af628020d99f7f36349f739960bdff4c6ee52ec
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-01-31 11:32:43 -0700

    chg batch size

--- models/streamline/external/balances/streamline__reward_points_realtime.sql
+++ models/streamline/external/balances/streamline__reward_points_realtime.sql
@@ -6,7 +6,7 @@
         params = {
             "external_table": "reward_points",
             "sql_limit": "32000",
-            "producer_batch_size": "1600",
+            "producer_batch_size": "8000",
             "worker_batch_size": "1600",
             "sql_source": "{{this.identifier}}"
         }
