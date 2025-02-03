2d66bb6c0f51c5454122bf3932dc568ff40d4b8e
Austin <austin@flipsidecrypto.com>
2025-02-03 12:19:02 -0500

    receipts

--- models/evm/streamline/core/history/streamline__get_evm_receipts_history.sql
+++ models/evm/streamline/core/history/streamline__get_evm_receipts_history.sql
@@ -4,7 +4,7 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"evm_receipts",
-        "sql_limit" :"25000",
+        "sql_limit" :"100000",
         "producer_batch_size" :"5000",
         "worker_batch_size" :"1000",
         "sql_source" :"{{this.identifier}}" }
@@ -12,57 +12,24 @@
     tags = ['streamline_history_evm']
 ) }}
 
-WITH last_3_days AS (
-
-    SELECT
-        ZEROIFNULL(block_number) AS block_number
-    FROM
-        {{ ref("_evm_block_lookback") }}
-),
-tbl AS (
+WITH tbl AS (
 
     SELECT
         block_number
     FROM
         {{ ref('streamline__evm_blocks') }}
-    WHERE
-        (
-            block_number >= (
-                SELECT
-                    block_number
-                FROM
-                    last_3_days
-            )
-        )
-        AND block_number IS NOT NULL
+    WHERE block_number IS NOT NULL
     EXCEPT
     SELECT
         block_number
     FROM
         {{ ref('streamline__complete_get_evm_receipts') }}
-    WHERE
-        block_number >= (
-            SELECT
-                block_number
-            FROM
-                last_3_days
-        )
-        AND _inserted_timestamp >= DATEADD(
-            'day',
-            -4,
-            SYSDATE()
-        )
 ),
 ready_blocks AS (
     SELECT
         block_number
     FROM
         tbl
-    UNION ALL
-    SELECT
-        block_number
-    FROM
-        {{ ref("_missing_receipts") }}
 )
 SELECT
     block_number,
