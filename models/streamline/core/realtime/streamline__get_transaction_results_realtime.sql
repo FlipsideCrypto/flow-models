a0c95745117523478724270161e7b98a5da4d205
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-04-02 09:38:43 -0600

    stage change

--- models/streamline/core/realtime/streamline__get_transaction_results_realtime.sql
+++ models/streamline/core/realtime/streamline__get_transaction_results_realtime.sql
@@ -7,13 +7,13 @@
     tags = ['streamline_realtime']
 ) }}
 
-WITH last_3_days AS ({% if var('STREAMLINE_RUN_HISTORY') %}
+WITH last_3_days AS (
+    {% if var('STREAMLINE_RUN_HISTORY') %}
 
     SELECT
         MAX(root_height) AS block_height
     FROM
         {{ ref('seeds__network_version') }}
-    {% else %}
     SELECT
         MAX(block_height) - 210000 AS block_height
     FROM
