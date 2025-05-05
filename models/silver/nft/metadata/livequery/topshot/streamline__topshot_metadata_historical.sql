0d80687302ba6cf6dac6d0cd74ab5a4e82fa424b
RAI <47441021+leningradtank@users.noreply.github.com>
2025-05-05 15:57:05 -0500

    added streamline changes

--- models/silver/nft/metadata/livequery/topshot/streamline__topshot_metadata_historical.sql
+++ models/silver/nft/metadata/livequery/topshot/streamline__topshot_metadata_historical.sql
@@ -1,6 +1,18 @@
 {{ config(
     materialized = 'view',
-    tags = ['streamline', 'topshot', 'moments_metadata', 'backfill']
+    tags = ['streamline', 'topshot', 'moments_metadata', 'backfill'],
+    post_hook = fsc_utils.if_data_call_function_v2(
+        func = '{{this.schema}}.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params = {
+            "external_table": "moments_minted_metadata_api",
+            "sql_limit": "100",
+            "producer_batch_size": "100",
+            "worker_batch_size": "100",
+            "sql_source": "{{this.identifier}}",
+            "async_concurrent_requests": "10"
+        }
+    )
 ) }}
 
 WITH api_parameters AS (
@@ -48,7 +60,7 @@ moments_to_backfill AS (
         WHERE
             event_contract = 'A.0b2a3299cc857e29.TopShot'
             AND event_type = 'MomentMinted'
-            AND block_timestamp::DATE = '2024-09-06'::DATE  
+            AND block_timestamp::DATE >= '2024-09-06'::DATE  
     ) mint_data
     ON m.moment_id = mint_data.moment_id
     WHERE
@@ -61,14 +73,25 @@ moments_to_backfill AS (
 )
 
 SELECT
+    DATE_PART('EPOCH', SYSDATE()) :: INTEGER AS partition_key,
     m.event_contract AS contract,
     m.moment_id AS id,
-    p.base_url,
-    OBJECT_CONSTRUCT(
-        'query', p.query,
-        'variables', OBJECT_CONSTRUCT('momentId', m.moment_id)
-    ) AS payload,
-    (SELECT last_date FROM last_metadata_date) AS last_metadata_date  -- For reference only
+    {{ target.database }}.live.udf_api(
+        'POST',
+        p.base_url,
+        {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Encoding': 'gzip',
+            'Connection': 'keep-alive',
+            'User-Agent': 'Flipside_Flow_metadata/0.1'
+        },
+        {
+            'query': p.query,
+            'variables': {'momentId': m.moment_id}
+        },
+        NULL
+    ) AS request
 FROM
     moments_to_backfill m
     CROSS JOIN api_parameters p
