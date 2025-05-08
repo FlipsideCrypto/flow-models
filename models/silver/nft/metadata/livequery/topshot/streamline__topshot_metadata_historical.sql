9680450326c40175eabf4e6ba843a778ed2f88bc
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2025-05-08 08:44:03 -0400

    SL work - proposed changes (#441)

--- models/silver/nft/metadata/livequery/topshot/streamline__topshot_metadata_historical.sql
+++ models/silver/nft/metadata/livequery/topshot/streamline__topshot_metadata_historical.sql
@@ -2,21 +2,20 @@
     materialized = 'view',
     tags = ['streamline', 'topshot', 'moments_metadata', 'backfill'],
     post_hook = fsc_utils.if_data_call_function_v2(
-        func = 'flow_dev.streamline.udf_bulk_rest_api_v2',
-target = "streamline.flow_dev.{{this.identifier}}",
-params = {
-    "external_table": "streamline.flow_dev.moments_minted_metadata_api",  
-            "sql_limit": "100",
-            "producer_batch_size": "100",
-            "worker_batch_size": "100",
-            "sql_source": "{{this.identifier}}",
-            "async_concurrent_requests": "10"
-        }
+        func = '{{this.schema}}.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table": "moments_minted_metadata_api",
+        "sql_limit": "100",
+        "producer_batch_size": "100",
+        "worker_batch_size": "100",
+        "sql_source": "{{this.identifier}}",
+        "async_concurrent_requests": "10" }
     )
 ) }}
 
 WITH api_parameters AS (
     -- Use the same parameters as for realtime
+
     SELECT
         base_url,
         query
@@ -25,72 +24,87 @@ WITH api_parameters AS (
     WHERE
         contract = 'A.0b2a3299cc857e29.TopShot'
 ),
-
 -- Get the most recent date we have metadata for
 last_metadata_date AS (
     SELECT
-        MAX(_inserted_timestamp)::DATE AS last_date
+        MAX(_inserted_timestamp) :: DATE AS last_date
     FROM
         {{ ref('silver__nft_topshot_metadata') }}
 ),
-
 -- Find all historical moments that need metadata
 moments_to_backfill AS (
     SELECT
         m.event_contract,
         m.moment_id
     FROM
-        {{ ref('livequery__topshot_moments_metadata_needed') }} m
-    LEFT JOIN (
-        SELECT 
-            moment_id,
-            COUNT(*) AS failure_count
-        FROM 
-            {{ ref('livequery__null_moments_metadata') }}
-        GROUP BY 
-            moment_id
-    ) null_attempts
-    ON m.moment_id = null_attempts.moment_id
-    LEFT JOIN (
-        SELECT
-            event_data:momentID::STRING AS moment_id,
-            block_timestamp
-        FROM
-            {{ ref('silver__nft_moments_s') }}
-        WHERE
-            event_contract = 'A.0b2a3299cc857e29.TopShot'
-            AND event_type = 'MomentMinted'
-            AND block_timestamp::DATE >= '2024-09-06'::DATE  
-    ) mint_data
-    ON m.moment_id = mint_data.moment_id
+        {{ ref('livequery__topshot_moments_metadata_needed') }}
+        m
+        LEFT JOIN (
+            SELECT
+                moment_id,
+                COUNT(*) AS failure_count
+            FROM
+                {{ ref('livequery__null_moments_metadata') }}
+            GROUP BY
+                moment_id
+        ) null_attempts
+        ON m.moment_id = null_attempts.moment_id
+        LEFT JOIN (
+            SELECT
+                event_data :momentID :: STRING AS moment_id,
+                block_timestamp
+            FROM
+                {{ ref('silver__nft_moments_s') }}
+            WHERE
+                event_contract = 'A.0b2a3299cc857e29.TopShot'
+                AND event_type = 'MomentMinted'
+                AND block_timestamp :: DATE >= '2024-09-06' :: DATE
+        ) mint_data
+        ON m.moment_id = mint_data.moment_id
     WHERE
-        COALESCE(null_attempts.failure_count, 0) < 3
-
+        COALESCE(
+            null_attempts.failure_count,
+            0
+        ) < 3
     ORDER BY
-        mint_data.block_timestamp ASC NULLS LAST,
-        CAST(m.moment_id AS INTEGER) ASC
-     LIMIT {{ var('SQL_LIMIT', 100) }} 
+        mint_data.block_timestamp ASC nulls last,
+        CAST(
+            m.moment_id AS INTEGER
+        ) ASC
+    LIMIT
+        {{ var(
+            'SQL_LIMIT', 100
+        ) }}
 )
-
-    SELECT
-    TO_CHAR(TO_TIMESTAMP_NTZ(SYSDATE()), 'YYYY_MM_DD') AS partition_key,
+SELECT
+    to_char(TO_TIMESTAMP_NTZ(SYSDATE()), 'YYYY_MM_DD') AS partition_key,
     m.event_contract AS contract,
     m.moment_id AS id,
-  {{ target.database }}.live.udf_api(
-    'POST',
-    p.base_url,
-    OBJECT_CONSTRUCT(
-      'Accept', 'application/json',
-      'Accept-Encoding', 'gzip',
-      'Connection', 'keep-alive',
-      'Content-Type', 'application/json',
-      'User-Agent', 'Flipside_Flow_metadata/0.1'
-    ),
-    OBJECT_CONSTRUCT(
-       'query', p.query,
-      'variables', OBJECT_CONSTRUCT('momentId', m.moment_id)
-    )
-) AS api_response
+    {{ target.database }}.live.udf_api(
+        'POST',
+        p.base_url,
+        OBJECT_CONSTRUCT(
+            'Accept',
+            'application/json',
+            'Accept-Encoding',
+            'gzip',
+            'Connection',
+            'keep-alive',
+            'Content-Type',
+            'application/json',
+            'User-Agent',
+            'Flipside_Flow_metadata/0.1'
+        ),
+        OBJECT_CONSTRUCT(
+            'query',
+            p.query,
+            'variables',
+            OBJECT_CONSTRUCT(
+                'momentId',
+                m.moment_id
+            )
+        )
+    ) AS request
 FROM
     moments_to_backfill m
     CROSS JOIN api_parameters p
