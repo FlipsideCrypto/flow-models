45a68debed36be9e41a4885fbf318c727dbb4b75
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-08-06 12:40:17 -0400

    AN-6231/modernize incremental predicates (#442)
    * upd incr predicate on silver core models
    
    * upd rest of the models
    
    * rm predicate from d+i model

--- models/silver/transfers/silver__token_transfers.sql
+++ models/silver/transfers/silver__token_transfers.sql
@@ -2,7 +2,7 @@
     materialized = 'incremental',
     incremental_strategy = 'merge',
     merge_exclude_columns = ['inserted_timestamp'],
-    incremental_predicates = ["COALESCE(DBT_INTERNAL_DEST.block_timestamp::DATE,'2099-12-31') >= (select min(block_timestamp::DATE) from " ~ generate_tmp_view_name(this) ~ ")"],
+    incremental_predicates = ["dynamic_range_predicate", "block_timestamp::DATE"],
     cluster_by = ['block_timestamp::date', 'modified_timestamp::date'],
     unique_key = "token_transfers_id",
     tags = ['scheduled_non_core']
