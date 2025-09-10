83880dd27c01b814c2b8390e4e9b58543f80d34a
stanz <gstanleytejakusuma@gmail.com>
2025-09-10 21:47:20 +0700

    bronze v2

--- models/bronze/streamline/realtime/bronze__streamline_fr_transaction_results.sql
+++ models/bronze/streamline/realtime/bronze__streamline_fr_transaction_results.sql
@@ -2,12 +2,9 @@
     materialized = 'view'
 ) }}
 
-{% set model = this.identifier.split("_")[-2:] | join('_') %}
-{{ streamline_external_table_FR_query(
+{{ streamline_external_table_FR_query_v2(
     model = 'transaction_results_v2',
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)",
-    partition_name = "_partition_by_block_id",
-    unique_key = "id"
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER)"
 ) }}
 
 
