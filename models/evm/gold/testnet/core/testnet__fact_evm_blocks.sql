5de5848aa3b13bafa8d7272fe3ee85664da54d1e
Jack Forgash <58153492+forgxyz@users.noreply.github.com>
2025-09-17 11:49:32 -0600

    add testnet to daily test (#486)

--- models/evm/gold/testnet/core/testnet__fact_evm_blocks.sql
+++ models/evm/gold/testnet/core/testnet__fact_evm_blocks.sql
@@ -3,7 +3,7 @@
     incremental_strategy = 'delete+insert',
     unique_key = "block_number",
     cluster_by = ['block_timestamp::DATE'],
-    tags = ['evm_testnet']
+    tags = ['evm_testnet','testnet_gold']
 ) }}
 
 SELECT
